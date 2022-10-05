b448fc184772574c85f53e566f68dd80c11aa9c7
grobnado <114610399+grobnado@users.noreply.github.com>
2022-10-06 00:21:17 +0300

    task 2

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 import gspread
 import numpy as np
+import matplotlib.pyplot as plt
+
+x = [3,21,22,34,54,34,55,67,89,99]
+x = np.array(x)
+y = [2,22,24,65,79,82,55,130,150,199]
+y = np.array(y)
+
+plt.scatter(x,y)
+def model(a, b, x):
+    return a*x + b
+
+#Tahe most commonly used loss function of linear regression model is the loss function of mean variance difference
+def loss_function(a, b, x, y):
+    num = len(x)
+    prediction=model(a,b,x)
+    return (0.5/num) * (np.square(prediction-y)).sum()
+
+#The optimization function mainly USES partial derivatives to update two parameters a and b
+def optimize(a,b,x,y):
+    num = len(x)
+    prediction = model(a,b,x)
+    #Update the values of A and B by finding the partial derivatives of the loss function on a and b
+    da = (1.0/num) * ((prediction -y)*x).sum()
+    db = (1.0/num) * ((prediction -y).sum())
+    a = a - Lr*da
+    b = b - Lr*db
+    return a, b
+
+#iterated function, return a and b
+def iterate(a,b,x,y,times):
+    for i in range(times):
+        a,b = optimize(a,b,x,y)
+    return a,b
+a = np.random.rand(1)
+b = np.random.rand(1)
+Lr = 0.000001
+
 gc = gspread.service_account(filename='unitydatasciense-364617-addbc6895ade.json')
 sh = gc.open("UnitySheets")
 price = np.random.randint(2000, 10000, 11)
@@ -10,10 +47,12 @@ while i <= len(mon):
     if i == 0:
         continue
     else:
-        tempInf = ((price[i-1]-price[i-2])/price[i-2])*100
+        a, b = iterate(a, b, x, y, 100)
+        prediction=model(a,b,x)
+        loss = loss_function(a, b, x, y)
+        tempInf = loss
         tempInf = str(tempInf)
         tempInf = tempInf.replace('.',',')
         sh.sheet1.update(('A' + str(i)), str(i))
-        sh.sheet1.update(('B' + str(i)), str(price[i-1]))
-        sh.sheet1.update(('C' + str(i)), str(tempInf))
+        sh.sheet1.update(('B' + str(i)), str(tempInf))
         print(tempInf)
